f61c6ffeeba9b138455a17ab29f7021b78449e40
vitorcunhacampos <vitorcunhacampos@gmail.com>
2021-05-27 11:08:35 -0300

    slide

---
+++ js/script.js
@@ -0,0 +1,15 @@
+new SimpleSlide({
+  slide: 'quote', // nome do atributo data-slide="principal"
+  nav: false, // se deve ou não mostrar a navegação
+  auto: true, // se o slide deve passar automaticamente
+  time: 5000, // tempo de transição dos slides
+  pauseOnHover: true, // pausa a transição automática
+});
+
+new SimpleSlide({
+  slide: 'portfolio', // nome do atributo data-slide="principal"
+  nav: true, // se deve ou não mostrar a navegação
+  auto: true, // se o slide deve passar automaticamente
+  time: 5000, // tempo de transição dos slides
+  pauseOnHover: true, // pausa a transição automática
+});
